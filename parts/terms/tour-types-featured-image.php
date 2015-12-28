7a9be6d4af4e9b3cdec906ae27f9cca0b06e0983
Norboo <istrate.paul@gmail.com>
2015-12-28 14:43:43 +0800

    Added Tour Type Featured Image

---
+++ parts/terms/tour-types-featured-image.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Title: Tour Types Featured Image
+Description: Add a featured image to your tour Category. How cool is that?
+Taxonomy: category
+Capability: manage_options
+Locked: true
+Collapse: true
+*/
+ 
+piklist('field', array(
+    'type' => 'file'
+    ,'field' => 'tour_types_featured_image'
+    ,'label' => 'Add File'
+    ,'description' => 'Upload only one file.'
+    ,'options' => array(
+      'modal_title' => __('Add File(s)','piklist')
+      ,'button' => __('Add','piklist')
+    )
+  ));
