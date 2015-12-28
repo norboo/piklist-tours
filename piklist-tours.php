dc0a34831027a4688fbdf5f1a5e0b5522f9dc80a
norboo <norboo@users.noreply.github.com>
2015-12-28 13:01:34 +0800

    Create piklist-tours.php

---
+++ piklist-tours.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Piklist Tours
+Plugin URI: http://eruisw.com
+Description: Create Tours
+Version: 1.0
+Author: Paul Istrate
+Author URI: http://eruisw.com
+Plugin Type: Piklist
+License: GPL2
+Domain Path: /languages
+Text Domain: piklist-tours
+GitHub Plugin URI: https://github.com/norboo/piklist-tours
+GitHub Branch: master
+*/
