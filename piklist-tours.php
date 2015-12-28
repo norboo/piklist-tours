f70f19a79fa9c9787c224566411b084dc95aba85
Norboo <istrate.paul@gmail.com>
2015-12-28 13:26:27 +0800

    Adding CPT and Custom Taxonomies

--- piklist-tours.php
+++ piklist-tours.php
@@ -13,3 +13,47 @@ Text Domain: piklist-tours
 GitHub Plugin URI: https://github.com/norboo/piklist-tours
 GitHub Branch: master
 */
+
+add_filter('piklist_post_types', 'custom_post_type');
+function custom_post_type($post_types)
+{
+    $post_types['tour'] = array(
+        'labels' => piklist('post_type_labels', 'Tours')
+        ,'title' => __('Enter a new Tour Title')
+        ,'public' => true
+        ,'rewrite' => array(
+            'slug' => 'tour'
+        )
+        ,'show_in_rest'       => true
+        ,'rest_base'          => 'tours'
+        ,'rest_controller_class' => 'WP_REST_Posts_Controller'
+        ,'supports' => array(
+            'title'
+            ,'excerpt'
+            ,'thumbnail'
+        )
+    );
+    return $post_types;
+}
+
+add_filter('piklist_taxonomies', 'custom_taxonomies');
+  function custom_taxonomies($taxonomies)
+  {
+    $taxonomies[] = array(
+      'post_type' => 'tour'
+      ,'name' => 'tour_type'
+      ,'configuration' => array(
+        'hierarchical' => false
+        ,'labels' => piklist('taxonomy_labels', 'Tour Types')
+        ,'page_icon' => plugins_url('parts/img/piklist-page-icon-32.png')
+        ,'show_ui' => true
+        ,'query_var' => true
+        ,'rewrite' => array( 
+          'slug' => '' 
+        )
+        ,'show_admin_column' => true
+        ,'comments' => true
+      )
+    );
+    return $taxonomies;
+  }
