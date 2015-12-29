45a258502664a2ab3829111e3aa624b2ed4e3915
Norboo <istrate.paul@gmail.com>
2015-12-29 20:33:16 +0800

    dec 29

--- piklist-tours.php
+++ piklist-tours.php
@@ -3,7 +3,7 @@
 Plugin Name: Piklist Tours
 Plugin URI: http://eruisw.com
 Description: Create Tours
-Version: 1.1
+Version: 1.2
 Author: Paul Istrate
 Author URI: http://eruisw.com
 Plugin Type: Piklist
@@ -23,6 +23,22 @@ function custom_post_type($post_types)
         ,'public' => true
         ,'rewrite' => array(
             'slug' => 'tour'
+        )
+        ,'show_in_rest'       => true
+        ,'rest_base'          => 'tours'
+        ,'rest_controller_class' => 'WP_REST_Posts_Controller'
+        ,'supports' => array(
+            'title'
+            ,'excerpt'
+            ,'thumbnail'
+        )
+        
+    $post_types['gallery'] = array(
+        'labels' => piklist('post_type_labels', 'Galleries')
+        ,'title' => __('Enter a new Gallery Title')
+        ,'public' => true
+        ,'rewrite' => array(
+            'slug' => 'gallery'
         )
         ,'show_in_rest'       => true
         ,'rest_base'          => 'tours'
@@ -55,5 +71,23 @@ add_filter('piklist_taxonomies', 'custom_taxonomies');
         ,'comments' => true
       )
     );
+    
+    $taxonomies[] = array(
+      'post_type' => 'gallery'
+      ,'name' => 'gallery_category'
+      ,'configuration' => array(
+        'hierarchical' => false
+        ,'labels' => piklist('taxonomy_labels', 'Gallery Categories')
+        ,'page_icon' => plugins_url('parts/img/piklist-page-icon-32.png')
+        ,'show_ui' => true
+        ,'query_var' => true
+        ,'rewrite' => array( 
+          'slug' => '' 
+        )
+        ,'show_admin_column' => true
+        ,'comments' => true
+      )
+    );
+    
     return $taxonomies;
   }
